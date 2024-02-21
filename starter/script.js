ce5c930bafc8580364cf8f079a79549700aa7d81
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-02-21 23:59:15 +0700

    forth

--- starter/script.js
+++ starter/script.js
@@ -11,5 +11,9 @@ document.querySelector('.guess').value = 23;
 */
 
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
 });
