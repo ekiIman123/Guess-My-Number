d9cae43d66c7ea2bb469247401e3e6cac550aa89
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-02-22 01:29:27 +0700

    sixth

--- starter/script.js
+++ starter/script.js
@@ -13,15 +13,23 @@ document.querySelector('.guess').value = 23;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // when player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Corret Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '25rem';
+
+    // when guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 To High!';
@@ -31,6 +39,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You Lose the Game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // when guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 To Low!';
