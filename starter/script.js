4c4c85ff13779b87c2b13d64352c1b6a322c6509
Eki Iman Jordiansyah <ekiimanjordiansyah212@gmail.com>
2024-02-21 23:43:04 +0700

    third

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,15 @@
 'use strict';
 
+/*
 document.querySelector('.message');
 document.querySelector('.message').textContent = 'Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
